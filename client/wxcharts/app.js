eaf56fdd1239f7be4978669c85f60187f94bc9ca
Damon_Huang <2006hua333@163.com>
2017-07-22 10:16:47 +0800

    前端简单修改

--- client/wxcharts/app.js
+++ client/wxcharts/app.js
@@ -12,14 +12,16 @@ App({
     wx.setStorageSync('logs', logs);
   },
   onShow: function () {
-    this.user_login();
-    console.log("fuck me ~~~~~")
-    //todo
+    var that = this;
+    console.log("on show");
+    that.user_login();
+    that.MessageHandler();
   },
   onHide: function () {
     //todo
   },
   user_login: function () {
+    console.log("user login~")
     wx.login({
       success: function (r) {
         var code = r.code;
@@ -34,20 +36,6 @@ App({
                     wx.sendSocketMessage({
                       data: JSON.stringify({ "from_id": "None", "from_group": "client", "to_id": 0, "to_group": "server", "msg": msg, "func": "user_login" }),
                     })
-                    wx.onSocketMessage(function (res) {
-                      var data = JSON.parse(res.data)
-                      console.log(data);
-                      if (data["from_id"] == 0 && data["func"] == "login") {
-                        var unionId = data["msg"];
-                        getApp().globalData.unionId = unionId;
-                        console.log(unionId);
-                        var req = ("db.user.update({'unionId':'{unionId1}'}," +
-                          "{'$setOnInsert':{'unionId':'{unionId2}'},'$addToSet':{'zxg':{'$each':['000001','399006']}}},upsert=True)").format({ "unionId1": unionId, "unionId2": unionId });
-                        wx.sendSocketMessage({
-                          data: JSON.stringify({ "from_id": unionId, "from_group": "client", "to_id": 2, "to_group": "server", "msg": req }),
-                        })
-                      }
-                    })
                   })
                 }
               })
@@ -57,8 +45,27 @@ App({
       }
     })
   },
-  globalData: {
-    unionId: "",
-    market: {}
+  MessageHandler: function () {
+    var that = this;
+    console.log("message handler~")
+    wx.onSocketMessage(function (res) {
+      var data = JSON.parse(res.data)
+      if (data["from_id"] == 0 && data["func"] == "login") {
+        var unionId = data["msg"];
+        that.Data.unionId = unionId;
+        var req = ("db.user.update({'unionId':'{unionId1}'}," +
+          "{'$setOnInsert':{'unionId':'{unionId2}'},'$addToSet':{'zxg':{'$each':['000001','399006']}}},upsert=True)").format({ "unionId1": unionId, "unionId2": unionId });
+        wx.sendSocketMessage({
+          data: JSON.stringify({ "from_id": unionId, "from_group": "client", "to_id": 2, "to_group": "server", "msg": req }),
+        })
+      }else if (data["from_id"]==1 && data["func"] == "send_market") {
+          var market = data["msg"];
+          that.Data.market = market; 
+      }
+    })
+  },
+  Data: {
+    unionId: null,
+    market: null,
   }
 });
--- client/wxcharts/pages/index/ZXG.js
+++ client/wxcharts/pages/index/ZXG.js
@@ -1,4 +1,7 @@
 // pages/index/ZXG.js
+var WxSearch = require('../../wxSearch/wxSearch.js')
+var app = getApp();
+
 Page({
 
   /**
@@ -17,22 +20,39 @@ Page({
   onLoad: function (options) {
 
     console.log("fuck you !!!!!!!!!!!!!!!!!!")
+	var that = this
+	WxSearch.init(that,43,['000001.SH','000002.SH','000001.SZ','000002.SZ']);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.interval = setInterval(this.fresh_market, 1000);
+  },
+  fresh_market: function () {
+    var that = this;
+    var gmarket = app.Data.market;
+    console.log("fuck the on ready!")
+    var market = new Array();
+    for (var code in gmarket) {
+      var name = gmarket[code][0];
+      var price = gmarket[code][3];
+      var pct_chg = 100*(gmarket[code][3] - gmarket[code][2])/gmarket[code][2];
+      pct_chg = pct_chg.toFixed(2); 
+      market.push({"code":code,"name":name,"price":price,"pct_chg":pct_chg}); 
+    }
+    console.log(market);
+    that.setData({
+      market: market,
+    })
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    while (1) {
-      console.log("i am in while 呵呵");
-    }
+
   },
 
   /**
@@ -46,7 +66,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    clearInterval(this.interval);
   },
 
   /**
@@ -68,6 +88,45 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  /**
+   * 添加自选股
+   */
+  addround: function(event) {
+    console.log("!!!clicked")
+  },  
+  wxSearchFn: function(e){
+    var that = this
+    WxSearch.wxSearchAddHisKey(that);
+    
+  },
+  wxSearchInput: function(e){
+    var that = this
+    WxSearch.wxSearchInput(e,that);
+  },
+  wxSerchFocus: function(e){
+    var that = this
+    WxSearch.wxSearchFocus(e,that);
+  },
+  wxSearchBlur: function(e){
+    var that = this
+    WxSearch.wxSearchBlur(e,that);
+  },
+  wxSearchKeyTap:function(e){
+    var that = this
+    WxSearch.wxSearchKeyTap(e,that);
+  },
+  wxSearchDeleteKey: function(e){
+    var that = this
+    WxSearch.wxSearchDeleteKey(e,that);
+  },
+  wxSearchDeleteAll: function(e){
+    var that = this;
+    WxSearch.wxSearchDeleteAll(that);
+  },
+  wxSearchTap: function(e){
+    var that = this
+    WxSearch.wxSearchHiddenPancel(that);
   }
 })
 
